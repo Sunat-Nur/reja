25c9b1994debdd5c7d67e4f05df46149212a5564
Sunat_nur <uzbekistan9699@gmail.com>
2023-09-30 15:26:38 +0900

    fix master branch

---
+++ app.js
@@ -0,0 +1,48 @@
+console.log("web serverni boshladik");
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+//
+// let user;
+// fs.readFile("database/user.json", "utf8",(err, data) => {
+//     if(err) {
+//         console.log("ERROR:", err);
+//     } else {
+//         user = JSON.parse(data)
+//     }
+// })
+
+// MongoDB call
+
+ const db = require("../../mt5project/reja/server").db();
+
+//1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// 2: Session code
+// 3: Views code
+
+app.set("views",   "views");
+app.set("view engine",  "ejs",);
+
+
+// 4 Routing code
+app.post("/create-item", (req, res) => {
+// TODO: code with db here
+
+});
+
+app.get ("/", function (req, res) {
+    res.render("reja");
+});
+
+//
+// app.get('/author', (req, res) => {
+//     res.render('author', {user: user})
+// })
+
+
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require( "http");
+const mongodb = require("mongodb");
+
+let db;
+const connectionString =
+    "mongodb+srv://sunat_nur:VxOnl9DbtZoyKFvx@cluster0.zkawn2h.mongodb.net/Reja"
+
+
+mongodb.connect(
+    connectionString,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err, client) => {
+        if(err) console.log("ERROR on connection MongoDB ");
+        else {
+            console.log("Mongo connection succeed");
+            module.exports = client;
+            const app = require("./app");
+            const server = http.createServer(app);
+            let PORT = 3000;
+            server.listen(PORT, function() {
+                console.log(
+                    `the server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+                );
+            });
+
+        }
+    }
+);
